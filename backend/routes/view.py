0d00bbadfb9a3e95e380ebb579921d057e977655
Keizo410 <iek427pop@outlook.jp>
2025-06-21 09:41:20 -0700

    merge conflict solved on local

--- backend/routes/view.py
+++ backend/routes/view.py
@@ -1,7 +1,6 @@
 from db.base_db import BaseDB
 from flask import Blueprint, jsonify, render_template
 import os
-from utilities.database import Database
 
 views_bp = Blueprint('views', __name__)
 # db = Database()
