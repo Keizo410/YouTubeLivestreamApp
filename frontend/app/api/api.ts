cd932928c5179ef98fc8bfe504bb7108f3175e70
Keizo Kato <90278067+Keizo410@users.noreply.github.com>
2025-06-21 10:27:49 -0700

    Update api.ts

--- frontend/app/api/api.ts
+++ frontend/app/api/api.ts
@@ -1,4 +1,4 @@
-const BACKEND_URL = process.env.EXPO_PUBLIC_LOCAL_BACKEND_URL;
+const BACKEND_URL = process.env.EXPO_PUBLIC_PROD_BACKEND_URL;
 
 export async function fetchYoutubers() {
   console.log("here ", BACKEND_URL);
