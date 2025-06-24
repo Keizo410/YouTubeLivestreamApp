5b2eb92414308589e3553a72fa62ac97000a3d29
Keizo410 <iek427pop@outlook.jp>
2025-06-24 12:07:52 -0700

    env var changed

--- backend/db/base_db.py
+++ backend/db/base_db.py
@@ -85,11 +85,11 @@ class BaseDB():
         Getter method for database connection.
         """
         return psycopg2.connect(
-            database=os.getenv("DATABASE"),
-            user=os.getenv("USER"),
-            password=os.getenv("PASSWORD"),
-            host=os.getenv("HOST"),
-            port=os.getenv("PORT")
+            database=os.getenv("POSTGRES_DATABASE"),
+            user=os.getenv("POSTGRES_USER"),
+            password=os.getenv("POSTGRES_PASSWORD"),
+            host=os.getenv("POSTGRES_HOST"),
+            port=os.getenv("POSTGRES_PORT")
         )
 
     def create_tables(self, filepath=None):
